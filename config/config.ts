3678fc93c5a9a7e03f9be7178207643d402ffc53
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-10-03 20:52:49 +0530

    created config file to handle global urls and other data specific to prod and dev environment

--- config/config.ts
+++ config/config.ts
@@ -6,7 +6,12 @@ export const config = {
 				url: "mongodb://localhost:27017/myBlogs",
 			},
 		},
-    // production configs come here.
-		prod: {},
+		// production configs come here.
+		prod: {
+			port: 8080,
+			dbConfig: {
+				url: `mongodb+srv://rishabh-mongo:mongodb-free@cluster0.v9a14sg.mongodb.net/myBlogs?retryWrites=true&w=majority`,
+			},
+		},
 	},
 };
